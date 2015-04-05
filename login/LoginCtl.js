ee0b314d6872154d38d9c9eb38cf6ab8a0110cf9
lakhansamani <lakhan.m.samani@gmail.com>
2015-04-05 09:24:20 +0530

    Adds static auth function in LoginCtl

--- login/LoginCtl.js
+++ login/LoginCtl.js
@@ -1,4 +1,14 @@
 var app=angular.module('auth-app.controllers');
 app.controller('LoginCtl',function($scope){
-
+	$scope.login=function(){
+		var data=$scope.user;
+		if(data.username === "admin@gmail.com" && data.password === "1234"){
+			$scope.success=true;
+			$scope.success_msg="Authenticated Successfully";
+		}
+		else{
+			$scope.err=true;
+			$scope.err_msg="Sorry invalid username or password";
+		}
+	}
 });
