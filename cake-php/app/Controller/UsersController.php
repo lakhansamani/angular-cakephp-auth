839c2d327c8cbaa4013319e948553cef2fc6152f
lakhansamani <lakhan.m.samani@gmail.com>
2015-04-07 14:23:58 +0530

    Adds auth logic for handling requests

--- cake-php/app/Controller/UsersController.php
+++ cake-php/app/Controller/UsersController.php
@@ -1,4 +1,21 @@
 <?php
+	class ResponseObject{
+		var $status = "";
+		var $data = array();
+		var $message = "";
+	}  // ResponseObject
+
+	class ErrorResponseObject{
+		var $status = "error";
+		var $data = -1;
+		var $message = "Sorry an unexpected error occurred.";
+	}  // ErrorResponseObject
+
+	class ErrorSessionFailed{
+		var $status = "error";
+		var $data = 3;
+		var $message = "Sorry an unexpected error occurred. please login again.";
+	}  // ErrorResponseObject
 	class UsersController extends AppController{
 
 		var $uses=array('User');
@@ -13,11 +30,42 @@
 			$this->response->header('Content-Type','application/json');
 		}
 		public function login(){
+			
 			//Add one user initially
+			
 			/*$user=array();
 			$user['email']="lakhan.m.samani@gmail.com";
 			$user['password']="123456";
 			$this->User->save($user);*/
+
+			$response_data = new ResponseObject();
+			if($this->request->is('post')){
+				$request_data=$this->request->input('json_decode',true);
+				$user=$this->User->find('first',array('conditions'=>array('email'=>$request_data['email'])));
+				if(sizeof($user)>0){
+					//check valid password
+					if($user['User']['password']==$request_data['password']){
+						// generate session code
+						$response_data->status='success';
+						$response_data->message='logged in';
+						//$request_data->data='sdfasdfas';
+					}
+					else{
+						$response_data->status='error';
+						$response_data->message='invalid password';
+					}
+				}	
+				else{
+					$response_data->status='error';
+					$response_data->message='no such user found';
+					//user not found
+				}
+			}
+			else{
+				$return_data = new ErrorResponseObject();
+			}
+			$this->response->body(json_encode($response_data));
+			return $this->response;
 		}
 	}
 ?>
--- cake-php/app/Model/User.php
+++ cake-php/app/Model/User.php
@@ -3,16 +3,22 @@
 		public function beforeSave($options=array()){
 			// hash our password
 			if (isset($this->data[$this->alias]['password'])) {
-				$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
-			}
-			
-			// if we get a new password, hash it
-			if (isset($this->data[$this->alias]['password_update'])) {
-				$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password_update']);
-			}
-		
+				 $this->data[$this->alias]['password'] = Security::rijndael($this->data[$this->alias]['password'], Configure::read('Security.salt'), 'encrypt');
+ 			}
 			// fallback to our parent
 			return parent::beforeSave($options);
+		}
+		public function afterFind($results, $primary = false) {
+		 
+		 // Decrypt private user data e.g. email and phone numbers
+		 foreach ($results as $key => $value){ 
+			 if(!empty($value[$this->alias]['password'])){
+			 	$results[$key][$this->alias]['password'] = Security::rijndael($value[$this->alias]['password'], Configure::read('Security.salt'),'decrypt');
+			 }
+		 }
+		 
+		 return $results;
+		 
 		}
 	}
 ?>
